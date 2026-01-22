78e47beb19ffeb3cc4626b0ce38ca8cef9367ba6
snorlaxGoodWin022 <wesleygoodwin022@gmail.com>
2026-01-22 19:59:37 +0800

    feat:模块化封装router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,35 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+ import { ElMessage } from 'element-plus';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 
-// 配置路由
-const routes: Array<RouteRecordRaw> = [{
-  path: '/',
-  name: 'Home',
-  component: () => import('@/views/home/index.vue'),
-  meta: {},
-  children: [],
-}];
+// import.meta.glob 为 vite 提供的特殊导入方式
+// 它可以将模块中全部内容导入并返回一个Record对象
+// 默认为懒加载模式 加入配置项 eager 取消懒加载
+const modules: Record<string, any> = import.meta.glob(['./modules/*.ts'], {
+  eager: true,// 立即加载，而非动态 import
+});
 
+const routes: Array<RouteRecordRaw> = [];
+
+// 将路由全部导入数组
+Object.keys(modules).forEach((key) => {
+  routes.push(modules[key].default);
+});
+
+//导入生成的路由数据
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
 
+// 全局前置守卫
 router.beforeEach(async (_to, _from, next) => {
-  NProgress.start();
+   // 可在此处添加权限校验、登录状态检查等逻辑
   next()
 });
 
+// 全局后置钩子
 router.afterEach((_to) => {
   NProgress.done();
 });
